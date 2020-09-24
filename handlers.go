f7b28f2d55a00053e37312936160fa16e5cc391f
Roman <rgeluz22@yahoo.com>
2020-09-24 14:56:14 -0700

    added content type and status for TodoIndex() for handlers.go

--- handlers.go
+++ handlers.go
@@ -24,6 +24,8 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 		Todo{Name: "Code app"},
 	}
 
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
